adfad525a078b10456eb9efb41819395c8817d05
Patrick Cremin <patrickfxcremin@gmail.com>
2020-06-02 21:02:08 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,14 +1,6 @@
 def introduction = (name)
   puts "Hi, my name is #{name}."
 end
-
-def introduction_with_language = (name, language)
- "Hi, my name is #{name} and I am learning to program in #{language}."
- end 
- 
- def introduction_with_language = (name, language = optional)
- "Hi, my name is #{name} and I am learning to program in #{language}."
- end 
  
  
 
